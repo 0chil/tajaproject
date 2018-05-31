aadaaca12f0964f11cc7df9f2ef3ce5dac367165
goraegori <39221443+goraegori@users.noreply.github.com>
2018-05-31 22:15:54 +0900

    긴글연습+자리연습

--- 타자연습.c
+++ 타자연습.c
@@ -16,6 +16,46 @@ int countdown(int t){
 }
 
 void positionT(){
+	srand(time(NULL));
+		int caps=rand()%2,alpha=rand()%26;
+		int answer=(caps?65:95)+alpha,input;
+		int prog=1,miss=0,hit=0;
+		double acc=0;
+	while(prog<=20){
+		if (hit>0)
+			acc=(hit-miss)/(double)hit;
+	system("clear");
+	printf("***********\n");
+	printf("     %c     \n",answer);
+	printf("***********\n");
+	printf("%d번째 입력\n",hit+1);
+	printf("진행도:%d/20\n 오타수:%d\n 정확도:%.2f%%\n",prog,miss,acc*100);
+	input=getch();
+	if(input==answer){
+	prog++;
+	hit++;
+	caps=rand()%2;
+	alpha=rand()%26;
+	answer=(caps?65:95)+alpha,input;
+	}
+	else if(input==27){
+		system("clear");
+		main();
+	}
+	else{
+		miss++;
+		hit++;
+	}
+	}
+	system("clear");
+	printf("전체타수:%d  , 오타수:%d , 정확도:%.2f%%\n",hit,miss,acc*100);
+	while(1){
+		input=getch();
+		if(input==13){
+			system("clear");
+			main();
+		}
+	}
 }
 
 void wordT(){
