5fc6e5626ec8f3e5b54d1c549a50277e82f72889
goraegori <39221443+goraegori@users.noreply.github.com>
2018-06-02 23:39:31 +0900

    Update 짧은글 연습.c

--- 짧은글 연습.c
+++ 짧은글 연습.c
@@ -15,6 +15,8 @@ void countdown(int t) {
 	printf("\b ");
 }
 
+int main(); //메인 함수로 돌아가기 위해
+
 void positionT() {
 	srand(time(NULL));
 	int caps = rand() % 2, alpha = rand() % 26;
@@ -29,7 +31,7 @@ void positionT() {
 		printf("     %c     \n", answer);
 		printf("***********\n");
 		printf("%d번째 입력\n", hit + 1);
-		printf("진행도:%d/20\n오타수:%d\n정확도:%.2f%%\n", prog, miss, acc * 100);
+		printf("진행도:%d%%\n오타수:%d\n정확도:%.2f%%\n", prog/20*100, miss, acc * 100);
 		input = getch();
 		if (input == answer) {
 			prog++;
@@ -66,17 +68,29 @@ void wordT() {
 char short_para[30][200] = { "To marry is to halve your rights and double your duties.","He makes no friend who never made a foe.","Common sense is the collection of prejudices acquired by age 18.","The unleashed power of the atom has changed everything save our modes of thinking, and we thus drift toward unparalleled catastrophes.","Nothing is more despicable than respect based on fear.","Television has proved that people will look at anything rather than each other.","It is only with the heart that one can see rightly; what is essential is invisible to the eye.","Love does not consist in gazing at each other, but in looking together in the same direction.","Business? It's quite simple. It's other people's money.","I would as soon leave my son a curse as the almighty dollar.","A great writer is, so to speak, a second government in his country. And for that reason no regime has ever loved great writers, only minor ones.","You don't live in a world all alone. Your brothers are here too.","A hungry man is not a free man.","To know is nothing at all; to imagine is everything.","Truth is generally the best vindication against slander.","It is my intention to present - through the medium of photography - intuitive observations of the natural world which may have meaning to the spectators.","America is a large, friendly dog in a very small room. Every time it wags its tail it knocks over a chair.","The secret of business is to know something that nobody else knows.","One man with courage makes a majority.","Television has a real problem. They have no page two.","Man is born to live, not to prepare for life. Life itself, the phenomenon of life, the gift of life, is so breathtakingly serious!","Like all great travellers, I have seen more than I remember, and remember more than I have seen.","The magic of first love is our ignorance that it can ever end.","When men are employed, they are best contented.","When she finally stopped conforming to the conventional picture of femininity she finally began to enjoy being a woman.","Anything you're good at contributes to happiness.","Accurst be he that first invented war.","Since a politician never believes what he says, he is surprised when others believe him.","A woman means by unselfishness chiefly taking trouble for others; a man means not giving trouble to others. Thus each sex regards the other as basically selfish.","Freedom is a system based on courage." };
 char short_input[5][200] = { 0 };
 int st_length[5] = { 0, };
+int corcnt=0;
 
-void ST_print(int randint, int T) {
+void ST_print(int randint, int T, int save) {
+	int son=0;
+	for(int i=0;i<=strlen(short_input[T]);i++){
+		if( (short_para[randint][i] == short_input[T][i]) && short_input[T][i] != '\0')
+		son++;
+	}
+	if(save==1){
+		corcnt+=son;
+	}
+	int mother=0;
+	for(int i=0;i<=T;i++){
+		mother+=strlen(short_input[i]);
+	}
 	system("cls");
-	//정확도, 현재타수 출력
-	printf("정확도 : %.0f%% 현재타수 : %d\n", 0.0, 0);
+	//진행도, 정확도, 최고타수, 현재타수 출력 
+	printf("진행도 : %.0f%% 정확도 : %.0f%% 최고타수 : %d 현재타수 : %d\n", (double)T/5*100, (double)(corcnt+son)/mother*100, 0, 0);
 	//정답 출력  
-	printf("%s\n", short_para[randint]);
+	printf("%s\n\n", short_para[randint]);
 	//지금까지 입력된 것 출력
 	printf("%s", short_input[T]);
 }
-int main(); //메인 함수로 돌아가기 위해 
 
 void SparagraphT() {
 	int T = 0;
@@ -84,32 +98,36 @@ void SparagraphT() {
 	for (int i = 0; i<5; i++)
 		for (int j = 0; j<200; j++)
 			short_input[i][j] = 0; //입력 배열 초기화  
-	int randint = rand() % 30, c /*입력 받는 변수(c)*/; //랜덤 지정 
-												  //각 줄의 글자수 셈 
-	for (int i = 0; i<10; i++)
-		st_length[i] = strlen(short_para[randint]) - 1;//1부터 센다(길이) 
-	ST_print(randint, T);
+	
 	while (T<5) {
-		if ((c = getch()) == 27) { //ESC 키  
-			system("cls");
-			main();
-		}
-		else if (c == '\b') { //백스페이스
-			int len = 0;
-			if (len = strlen(short_input[T]) > 0) {
-				short_input[T][len - 1] = '\0';
-				ST_print(randint, T);
+		int randint = rand() % 30, c /*입력 받는 변수(c)*/; //랜덤 지정
+		for (int i = 0; i<5; i++)
+			st_length[i] = strlen(short_para[randint]) - 1;//1부터 센다(길이) 
+		ST_print(randint, T, 0);
+		while(strlen(short_input[T]) <= st_length[T]){
+			if ((c = getch()) == 27) { //ESC 키  
+				system("cls");
+				main();
+			}
+			else if (c == '\b') { //백스페이스
+				int len = strlen(short_input[T]);
+				if (len > 0) {
+					short_input[T][len] = NULL;
+					short_input[T][len - 1] = NULL;
+					ST_print(randint, T, 0);
+				}
+			}
+			else {
+				short_input[T][strlen(short_input[T])] = c;
+				ST_print(randint, T, 0);
 			}
 		}
-		else {
-			short_input[T][strlen(short_input[T])] = c;
-			ST_print(randint, T);
-		}
+		ST_print(randint, T, 1);
 		T++;
 	}
 }
 
-char long_para[4][10][100] = { "The Middle\n","Take a seat right over there, sat on the stairs\n","Stay or leave, the cabinets are bare and I'm unaware\n","Of just how we got into this mess, got so aggressive\n","I know we meant all good intentions\n", \
+char long_para[4][10][100] = {"The Middle\n","Take a seat right over there, sat on the stairs\n","Stay or leave, the cabinets are bare and I'm unaware\n","Of just how we got into this mess, got so aggressive\n","I know we meant all good intentions\n", \
 "So pull me closer\n","Why don't you pull me close?\n","Why don't you come on over?\n","I can't just let you go\n","Oh baby, why don't you just meet me in the middle?\n", \
 
 "Faint\n","I am a little bit of loneliness, a little bit of disregard\n","Handful of complaints but I can't help the fact\n","That everyone can see these scars\n","I am what I want you to want, what I want you to feel\n", \
@@ -119,13 +137,12 @@ char long_para[4][10][100] = { "The Middle\n","Take a seat right over there, sat
 "I wanna feel the way that we did that summer night\n","Drunk on a feeling, alone with the stars in the sky\n","I've been running through the jungle\n","I've been running with the wolves\n","To get to you, to get to you\n", \
 
 "Butterflies And Hurricanes\n","Change everything you are\n","And everything you were\n","Your number has been called\n","Fights and battles have begun\n", \
-"Revenge will surely come\n"," Your hard times are ahead\n","Best, You've got to be the best\n","You've got to change the world\n","And use this chance to be heard\n" };
+"Revenge will surely come\n"," Your hard times are ahead\n","Best, You've got to be the best\n","You've got to change the world\n","And use this chance to be heard\n"};
 char long_input[10][100] = { 0 };
 int LT_length[10] = { 0 };
 int LT_ln = 0, LT_col = 0;
 
 int LT_count_answers(int randint) {
-	if (LT_ln == 0 && LT_col == 0) return 0;//처음 실행시 0%로 표시
 	int corcnt = 0;
 	for (int i = 0; i <= LT_ln; i++)
 		for (int j = 0; j <= LT_length[i]; j++)
@@ -135,7 +152,6 @@ int LT_count_answers(int randint) {
 }
 
 int LT_count_inputs() {
-	if (LT_ln == 0 && LT_col == 0) return 1; //처음 실행시 0%로 표시  
 	int cnt = 0;
 	for (int i = 0; i <= LT_ln; i++)
 		for (int j = 0; j <= LT_length[i]; j++)
@@ -146,16 +162,20 @@ int LT_count_inputs() {
 
 void LT_print(int randint, int ln, int col) {
 	system("cls");
-	printf("%d %d\n", LT_ln, LT_col);
+	//printf("%d %d\n", LT_ln, LT_col); //현 위치 체크(디버깅) 
 	//정확도, 현재타수 출력
 	printf("정확도 : %.0f%% 현재타수 : %d\n", (float)LT_count_answers(randint) / LT_count_inputs() * 100, 0);
 	//정답 출력  
-	if (ln<5)
+	if (ln<5){
 		for (int i = 0; i<5; i++)
 			printf("%s", long_para[randint][i]);
-	else
+		printf("\n");
+	}
+	else{
 		for (int i = 5; i<10; i++)
 			printf("%s", long_para[randint][i]);
+		printf("\n");
+	}
 	//지금까지 입력된 것 출력
 	if (ln<5)
 		for (int i = 0; i <= ln; i++)
